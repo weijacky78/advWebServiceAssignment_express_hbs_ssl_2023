eeafe39689407d70310e13f54530957fd21e5780
Xin <weijacky78@hotmail.com>
2023-05-12 13:08:09 -0400

    regenerated a new weather api key

--- routes/weather.js
+++ routes/weather.js
@@ -8,7 +8,7 @@ router.use(cors());
 /* GET home page. */
 router.get('/', async function (req, res, next) {
 
-    let url = `https://api.weatherapi.com/v1/forecast.json?key=9a4b327b695b412c92e183028231105 &q=Ottawa, Canada&aqi=no`;
+    let url = `http://api.weatherapi.com/v1/forecast.json?key=98e9f5f3121141e1b07162521231205 &q=Ottawa, Canada&aqi=no`;
     let fetch = await cache.fetchUrl(url);
     let out = {
         "date": fetch.current.last_updated,
