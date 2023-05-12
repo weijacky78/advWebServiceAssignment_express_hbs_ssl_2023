d5851c873bbc1db941b22d1874cad38f4f3cfe38
Xin <weijacky78@hotmail.com>
2023-05-12 09:24:40 -0400

    added the wIcon te represent the weather condition image

--- routes/weather.js
+++ routes/weather.js
@@ -9,14 +9,12 @@ router.use(cors());
 router.get('/', async function (req, res, next) {
 
     let url = `https://api.weatherapi.com/v1/forecast.json?key=9a4b327b695b412c92e183028231105 &q=Ottawa, Canada&aqi=no`;
-    console.log("hello", url)
     let fetch = await cache.fetchUrl(url);
-    console.log("hello", fetch)
     let out = {
         "date": fetch.current.last_updated,
+        "wIcon": fetch.current.condition.icon,
         "wDescriptions": fetch.current.condition.text,
         "temperature": fetch.current.temp_c,
-
 
     };
     res.json(out);
